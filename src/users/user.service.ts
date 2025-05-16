b244eb0668acd08d0b4044bab42d9f7703053779
shivamdeswal5 <deswalworks@gmail.com>
2025-05-16 17:49:12 +0530

    nest basics

---
+++ src/users/user.service.ts
@@ -0,0 +1,19 @@
+export class UsersService{
+    users: {id:number,name:string,age:number,gender:string,isMarried: boolean} [] = [
+        {id:1,name:'Shivam', age: 21, gender:'male',isMarried:false},
+        {id:2,name:'Deswal', age: 21, gender:'male',isMarried:false},
+    ]
+
+    getUsers(){
+        return this.users;
+    }
+
+    getUserById(id:number){
+        return this.users.find((user)=> user.id === id)
+    }
+
+    createUsers(user: {id:number,name:string,age:number,gender:string,isMarried: boolean}){
+        this.users.push(user);
+        return "User Created Successfully"
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { UsersService } from "./user.service";
 
 @Controller('users')
 export class UsersController {
     @Get()
-    getUsers(){
-        return 'Get Request to to get all users'
+    getUsers(@Query() query:any){
+        const usersServices = new UsersService();
+        console.log(query);
+        return usersServices.getUsers();
+
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') param :string){
+        const id = parseInt(param);
+        const usersServices = new UsersService();
+        return usersServices.getUserById(id);
     }
 
     @Post()
     createUsers(){
-        return 'A new user has been created'
+        const user = {id:3,name:'Deswal', age: 21, gender:'male',isMarried:false}
+        const usersServices = new UsersService();
+        return usersServices.createUsers(user);
     }
 
 }
